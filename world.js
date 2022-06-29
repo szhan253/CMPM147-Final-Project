5e7fd192d54a8c23013ac0d3bd16d0f49923ca1f
julinas <julin0030@gmail.com>
2022-06-28 19:06:27 -0700

    Create world.js

---
+++ world.js
@@ -0,0 +1,85 @@
+"use strict";
+
+/* global XXH */
+/* exported --
+    p3_preload
+    p3_setup
+    p3_worldKeyChanged
+    p3_tileWidth
+    p3_tileHeight
+    p3_tileClicked
+    p3_drawBefore
+    p3_drawTile
+    p3_drawSelectedTile
+    p3_drawAfter
+*/
+
+function p3_preload() {}
+
+function p3_setup() {}
+
+let worldSeed;
+
+function p3_worldKeyChanged(key) {
+  worldSeed = XXH.h32(key, 0);
+  noiseSeed(worldSeed);
+  randomSeed(worldSeed);
+}
+
+function p3_tileWidth() {
+  return 16;
+}
+function p3_tileHeight() {
+  return 16;
+}
+
+let [tw, th] = [p3_tileWidth(), p3_tileHeight()];
+
+let clicks = {};
+
+function p3_tileClicked(i, j) {
+  let key = [i, j];
+  clicks[key] = 1 + (clicks[key] | 0);
+}
+
+function p3_drawBefore() {}
+
+function p3_drawTile(i, j) {
+  noStroke();
+  fill(noise(i, j) * 255)
+
+  push();
+
+  beginShape();
+  vertex(0, 0);
+  vertex(0, tw);
+  vertex(th, tw);
+  vertex(th, 0);
+  endShape(CLOSE);
+
+  let n = clicks[[i, j]] | 0;
+  if (n % 2 == 1) {
+    fill(255, 255, 0, 180);
+    ellipse(th/2, tw/2, 10, 10);
+  }
+
+  pop();
+}
+
+function p3_drawSelectedTile(i, j) {
+  noFill();
+  stroke(0, 255, 0, 128);
+
+  beginShape();
+  vertex(0, 0);
+  vertex(0, tw);
+  vertex(th, tw);
+  vertex(th, 0);
+  endShape(CLOSE);
+
+  noStroke();
+  fill(0);
+  text("tile " + [i, j], 0, 0);
+}
+
+function p3_drawAfter() {}
