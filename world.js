56e4c42d63ff2b03f8757514d3bb687e696af507
sop <szhan253@ucsc.edu>
2022-06-30 19:12:39 -0700

    changed all block color

--- world.js
+++ world.js
@@ -27,10 +27,10 @@ function p3_worldKeyChanged(key) {
 }
 
 function p3_tileWidth() {
-  return 16;
+  return 32;
 }
 function p3_tileHeight() {
-  return 16;
+  return 32;
 }
 
 let [tw, th] = [p3_tileWidth(), p3_tileHeight()];
@@ -47,19 +47,31 @@ function p3_drawBefore() {}
 
 function p3_drawTile(i, j) {
   noStroke();
+  //background color of cubes
   fill(noise(i, j) * 255)
 
   push();
 
-  beginShape();
-  vertex(0, 0);
-  vertex(0, tw);
-  vertex(th, tw);
-  vertex(th, 0);
-  endShape(CLOSE);
+  // beginShape();
+  // vertex(0, 0);
+  // vertex(0, tw);
+  // vertex(th, tw);
+  // vertex(th, 0);
+  // endShape(CLOSE);
+
+  let rand = noise(i, j);
+
+  if(rand < 0.33){
+    drawGrass(i, j);
+  }else if(rand < 0.66 && rand >= 0.33){
+    drawRock();
+  }else{
+    drawBlock();
+  }
 
   let n = clicks[[i, j]] | 0;
   if (n % 2 == 1) {
+    //clicked circle's color
     fill(255, 255, 0, 180);
     ellipse(th/2, tw/2, 10, 10);
   }
@@ -67,6 +79,8 @@ function p3_drawTile(i, j) {
   pop();
 }
 
+
+//green frame showing which block is slected by mouse
 function p3_drawSelectedTile(i, j) {
   noFill();
   stroke(0, 255, 0, 128);
@@ -84,3 +98,21 @@ function p3_drawSelectedTile(i, j) {
 }
 
 function p3_drawAfter() {}
+
+function drawGrass(i, j){
+  fill(138, 163, 43);
+  noStroke();
+  rect(0, 0, th, tw);
+}
+
+function drawRock(){
+  fill("#AACAF5");
+  noStroke();
+  rect(0, 0, th, tw);
+}
+
+function drawBlock(){
+  fill("#9F77FF");
+  noStroke();
+  rect(0, 0, th, tw);
+}
